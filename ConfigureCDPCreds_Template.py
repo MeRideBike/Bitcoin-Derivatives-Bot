d20cb6361fe27f66d7a6f0fa6d943f5c1bc9c780
Ethan Bianchi <casper@meridebike.org>
2025-03-23 17:28:33 -0400

    Added: Configure Credentials (Template) Script
    Replace w/ your own credentials for TradingEnv (Python Initialize Environment) Script to function properly.

---
+++ ConfigureCDPCreds_Template.py
@@ -0,0 +1,9 @@
+from cdp import *
+
+api_key_name = "GOES HERE"
+
+api_key_private_key = "GOES HERE"
+
+Cdp.configure(api_key_name, api_key_private_key)
+
+print("CDP SDK has been successfully configured with CDP API key.")
